1eeaa1670ce8447607c15ab59be49722394ff862
sam <wax654@gmail.com>
2021-02-16 15:39:16 -0800

    mades the survey start page

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template as render, request
-from flask_debugtoolbar import flask_debugtoolbar
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
 
 app = Flask(__name__)
-app.config['secret_key'] = 'a secret key'
+
+app.config['SECRET_KEY'] = 'a secret key'
+toolbar = DebugToolbarExtension(app)
 responses = []
+
+@app.route('/')
+def survey_start():
+    return render('survey_start.html', survey=satisfaction_survey)
