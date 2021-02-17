2fcb8e077c8bdb4d0230efdf37be6480e1f01c0e
sam <wax654@gmail.com>
2021-02-17 13:53:18 -0800

    Session variables

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template as render, request, redirect, flash
+from flask import Flask, render_template as render, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -12,27 +12,33 @@ app = Flask(__name__)
 
 #For the debugger toolbar
 app.config['SECRET_KEY'] = 'a secret key'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
-
-#responses
-responses = []
-curr_question = 0
 
 
 @app.route('/')
-def survey_start():
+def index():
     return render('survey_start.html', survey=survey)
+
+
+@app.route('/start_survey', methods=["POST"])
+def survey_start():
+    session['responses'] = []
+    return redirect('questions/0')
+
 
 @app.route('/questions/<int:q_id>')
 def question(q_id):
+    curr_question = len(session['responses'])
     #not the right question
     if q_id != curr_question:
         flash("let's not try to make trouble here. Let's just finish the suvery ok?")
         return redirect(f"/questions/{curr_question}")
     
-    #all q's are answered the end of the survey
-    if len(survey.questions) <= len(responses):
+    #all q's are answered - The end of the survey
+    if len(survey.questions) <= curr_question:
         return redirect('/thankyou')
+    
     #try to get the question
     try:
         question = survey.questions[q_id]
@@ -42,14 +48,17 @@ def question(q_id):
 
 @app.route('/answer', methods=["POST"])
 def answer():
-    global curr_question
+    curr_question = len(session['responses'])
     
-    res = request.form.get('choice')
-    if not res:
+    answer = request.form.get('choice')
+    if not answer:
         flash("Please answer the Question!!!")
         return redirect(f"/questions/{curr_question}")
-    responses.append(res)
-    curr_question = len(responses)
+    
+    responses = session['responses']
+    responses.append(answer)
+    session['responses'] = responses
+    curr_question = len(session['responses'])
     return redirect(f"/questions/{curr_question}")
 
 @app.route('/thankyou')
